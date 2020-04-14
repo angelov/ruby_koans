3b1d992347da59d401c0244b630d93983dd51159
Dejan Angelov <angelovdejan92@gmail.com>
2020-04-14 19:49:55 +0200

    125..179

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,16 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  [a, b, c].each { |side| raise TriangleError unless side.positive? }
+
+  raise TriangleError unless (a + b > c) && (a + c > b) && (b + c > a)
+
+  unique_sides = [a, b, c].uniq
+  case unique_sides.length
+    when 1 then :equilateral
+    when 2 then :isosceles
+    else :scalene
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
